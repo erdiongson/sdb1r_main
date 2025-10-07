bcea8b714e912ab87ee2ee69dacaa216004cd623
Ryan Yu <ryanyudev@gmail.com>
2025-10-07 12:46:13 +0800

    fix: password errors

--- src/controllers/ProfileController.cpp
+++ src/controllers/ProfileController.cpp
@@ -48,12 +48,13 @@ void ProfileController::on_interaction(const Interaction& interaction) {
       break;
 
     case TAG_PROFILE_CHANGE_PASSWORD:
-      char new_password_1[PASSWORD_MAX_LEN];
-      char new_password_2[PASSWORD_MAX_LEN];
+      char new_password_1[PASSWORD_MAX_LEN] = "";
+      char new_password_2[PASSWORD_MAX_LEN] = "";
       
       Keyboard(phost, new_password_1, "Enter New Password", FALSE);
       if (new_password_1[0] == 0) {
         Keyboard(phost, "Error : No password entered", "Press Back to continue", FALSE);
+        DisplayProfileMenu(0, selectedProfileNum, CurProf);
         return;
       }
 
@@ -61,11 +62,13 @@ void ProfileController::on_interaction(const Interaction& interaction) {
 
       if (strcmp(new_password_1, new_password_2) != 0) {
         Keyboard(phost, "Error : Different password entered", "Press Back to continue", FALSE);
+        DisplayProfileMenu(0, selectedProfileNum, CurProf);
         return;
       }
 
       WritePassEEPROM(new_password_1);
       Keyboard(phost, "Password changed", "Press Back to continue", FALSE);
+      DisplayProfileMenu(0, selectedProfileNum, CurProf);
       break;
 
     default:
