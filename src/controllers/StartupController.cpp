9c629226046545ed6164949403423aef2c6e2650
Ryan Yu <ryanyudev@gmail.com>
2025-10-08 12:52:59 +0800

    feat: update startup to use correct directions

--- src/controllers/StartupController.cpp
+++ src/controllers/StartupController.cpp
@@ -23,23 +23,27 @@ ControllerStepResult StartupController::on_step() {
   if (result.dispenser == DISPENSER_STATE_ERROR_ACK_ERROR) {
     draw_logo_screen(phost, ERROR_ACK_ERROR);
     stage = STAGE_ERROR;
+    return ControllerStepResult(-1, -1);
   }
   if (result.dispenser == DISPENSER_STATE_ERROR_MARKER_NOT_DETECTED) {
     draw_logo_screen(phost, ERROR_MARKER_NOT_DETECTED);
     stage = STAGE_ERROR;
+    return ControllerStepResult(-1, -1);
   }
   if (result.dispenser == DISPENSER_STATE_ERROR_IR_SENSOR_FAILURE) {
     draw_logo_screen(phost, ERROR_IR_SENSOR);
     stage = STAGE_ERROR;
+    return ControllerStepResult(-1, -1);
   }
   if (result.steppers == AXIS_STATE_ERROR_LIMIT_SWITCH) {
     draw_logo_screen(phost, ERROR_LIMIT_SWITCH);
     stage = STAGE_ERROR;
+    return ControllerStepResult(-1, -1);
   }
 
   if (stage == STAGE_HANDSHAKE && result.dispenser == DISPENSER_STATE_IDLING) {
     Dprint(F("Handshake acknowledged 👍"));
-    if (dispenserHead.x().isAtMax()) dispenserHead.x().moveBy(-STEPS_PER_UNIT_X * 30);
+    if (dispenserHead.x().isAtMin()) dispenserHead.x().moveBy(STEPS_PER_UNIT_X * 30);
     if (dispenserHead.y().isAtMin()) dispenserHead.y().moveBy(STEPS_PER_UNIT_Y * 30);
     if (dispenserHead.z().isAtMin()) dispenserHead.z().moveBy(STEPS_PER_UNIT_Z * 30);
     
@@ -49,7 +53,7 @@ ControllerStepResult StartupController::on_step() {
   
   if (stage == STAGE_CLEAR && result.steppers == AXIS_STATE_COMPLETE) {
     Dprint(F("Axis cleared 👍"));
-    dispenserHead.x().moveToMax();
+    dispenserHead.x().moveToMin();
     dispenserHead.y().moveToMin();
     dispenserHead.z().moveToMin();
     stage = STAGE_HOME;
